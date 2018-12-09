e356c267bd69619d7b2925ead46e364168c84829
Daisuke Sato <tiryoh@gmail.com>
2018-12-09 15:42:23 +0900

    test
    ```
    $ python -m flake8 test.py
    test.py:4:8: E225 missing whitespace around operator
    test.py:5:1: E302 expected 2 blank lines, found 0
    test.py:7:1: E305 expected 2 blank lines after class or function definition, found 0
    ```

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-VERSION = "0.0.1"
-
+VERSION="0.0.1"
 def main():
     print("main")
-
-
 if __name__ == '__main__':
     print("Version " + VERSION)
     main()
